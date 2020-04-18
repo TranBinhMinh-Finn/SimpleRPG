845f0bc95f87e130c07bb28f9d499ba6e376896d
finefinnfinning <finefinnfinning@gmail.com>
2020-04-19 00:33:04 +0700

    base libgdx project

---
+++ desktop/src/com/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.SimpleRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimpleRPG(), config);
+	}
+}
