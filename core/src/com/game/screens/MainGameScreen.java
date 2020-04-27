ea3a40d04b1409d1e89e5bd7251cbe61743fddc1
TranMinhCong <congtraninf@gmail.com>
2020-04-27 16:51:49 +0700

    testing...

--- core/src/com/game/screens/MainGameScreen.java
+++ core/src/com/game/screens/MainGameScreen.java
@@ -10,13 +10,11 @@ public class MainGameScreen implements Screen {
 
 	SimpleRPG game;
 	Player danchoi1;
-	Player danchoi2;
 	Big_Demon quai1;
 	public MainGameScreen(SimpleRPG game)
 	{
 		this.game = game;
 		danchoi1 = new Player(game,0,0);
-		danchoi2 = new Player(game,Player.CHAR_WIDTH*Player.scale,Player.CHAR_HEIGHT*Player.scale);
 		quai1 = new Big_Demon(game,800,600);
 	}
 	
@@ -32,7 +30,6 @@ public class MainGameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		float del = Gdx.graphics.getDeltaTime();
 		danchoi1.inputQuery(del);
-		danchoi2.inputQuery(del);
 		quai1.actionQuery(del);
 		
 	}
